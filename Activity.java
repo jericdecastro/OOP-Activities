d189615ec4ad0c5eaea3576c803e845becd61251
jericdecastro <jekteezy648@gmail.com>
2024-09-20 10:14:20 +0800

    Update Activity.java

--- Activity.java
+++ Activity.java
@@ -1,8 +1,19 @@
-class Activity{
-    
-    public Dog(String name, boolean isMale){
-        dog.name = 
-        dog.isMale =
-        dog.puppies =
+public class Activity{
+    public static void main(String[] args ){
+        Dog aso = new Dog();
+        aso.setName("Max");
+        aso.puppies = 3;
+
+        aso.doggy[0]= "Whitey";
+        aso.doggy[1]= "dogger man";
+        aso.doggy[2]= "Uno";
+
+        System.out.println(aso.name());
+        System.out.println(aso.bark());
+        System.out.println(aso.showNumberOfPuppies());
+
+        for (int i = 0; i < 3; i++){
+            System.out.println("Puppy: "+ aso.doggy[i]);
+        }
     }
-    }
+}
